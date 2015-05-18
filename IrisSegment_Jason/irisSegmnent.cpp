79c2c9e63742cf10e52339e602f62bc833fca329
jinh1990 <jinh1990@gmail.com>
2015-05-18 16:04:21 +0800

    11111

--- IrisSegment_Jason/irisSegmnent.cpp
+++ IrisSegment_Jason/irisSegmnent.cpp
@@ -309,5 +309,5 @@ void cal_canny_high_thresd(Mat src, float ratio, int& high_thresd)
 	}
 	cout<<"high_thresd = "<<high_thresd<<endl;
 
-	cout<<"It's the lastest version."<<endl;
+	cout<<"It's not the lastest version."<<endl;
 }
